b0e9152c85e0850fe55073dad8c06bf9eb01f2a1
bertronika <nejc@bertoncelj.eu.org>
2024-02-23 16:15:16 +0100

    Dodaj manjkajoče parametre demonstracijskim skriptom

--- demo/primer1.m
+++ demo/primer1.m
@@ -1,12 +1,13 @@
-% Demonstracija steganofonskega dekodiranja brez uporabe izvornega posnetka
-
 cd ..
 source("param.octave");
 
 disp("\n== Demonstracija dekodiranja brez uporabe izvornega posnetka ==");
 
+sf.en.input_file   = "demo/skrivnost.txt";
+sf.en.input_audio  = "demo/400Hz30.wav";
 sf.en.output_audio = "demo/skrivnost1.wav";
 sf.de.use_input_audio = false;
+
 sf.param.frame_len = 1024*4;
 sf.param.rand_seed = 1932;
 sf.param.rand_func = @randn;
--- demo/primer2.m
+++ demo/primer2.m
@@ -1,12 +1,13 @@
-% Demonstracija steganofonskega dekodiranja z uporabo izvornega posnetka
-
 cd ..
 source("param.octave");
 
 disp("\n== Demonstracija dekodiranja z uporabo izvornega posnetka ==");
 
+sf.en.input_file   = "demo/skrivnost.txt";
+sf.en.input_audio  = "demo/400Hz30.wav";
 sf.en.output_audio = "demo/skrivnost2.wav";
 sf.de.use_input_audio = true;
+
 sf.param.frame_len = 1024*4;
 sf.param.rand_seed = 1932;
 sf.param.rand_func = @randn;
