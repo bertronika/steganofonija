4ed59cf3cccdf8468c165957f69440e9dbd7fc69
bertronika <nejc@bertoncelj.eu.org>
2024-03-18 21:38:03 +0100

    Spremeni analizni skript v funkcijo

--- analiziraj.m
+++ analiziraj.m
@@ -1,18 +1,94 @@
 % Časovna in spektralna analiza zvočnih posnetkov
 
-pkg load signal;
+function analiziraj(file1=0, file2=0)
+	pkg load signal;
 
-if (exist("VALIDATING_CODING", "var") != 1)
-	# au1 = "glas/galliard.wav";
-	au1 = "demo/400Hz30.wav";
-	au2 = "demo/skrivnost.wav";
+	global sf;
 
-	[x, fs] = audioread(au1);
-	[y, ~] = audioread(au2);
-	# x_info = audioinfo(au1);
-else
-	SAV = sf.param.save_graphs_to_files;
-endif
+	if (file1 != 0 && file2 != 0)
+		[x, fs]  = audioread(file1);
+		[y, ~]   = audioread(file2);
+		channels = audioinfo(file1).NumChannels;
+	else
+		global x y fs channels;
+	endif
+
+	close all;
+
+	disp("-- Analiza signalov ---------");
+	tic;
+
+	% Če ima posnetek dva kanala, za izdelavo analize izračunaj njuno povprečje
+	if (channels == 2)
+		x_averaged = (x(:,1) + x(:,2))/2;
+		y_averaged = (y(:,1) + y(:,2))/2;
+	else
+		x_averaged = x;
+		y_averaged = y;
+	endif
+
+	if (numel(x)/fs > 60)
+		disp("# Omejujem dolžino posnetkov na 60 s pri FFT analizah");
+		x_reduced = x_averaged(1:60 * fs);
+		y_reduced = y_averaged(1:60 * fs);
+	else
+		x_reduced = x_averaged;
+		y_reduced = y_averaged;
+	endif
+
+	if (sf.param.save_graphs_to_files)
+		H1 = figure("visible", "off");
+	else
+		H1 = figure;
+	endif
+	pos = get(H1, 'Position');
+	set(H1, 'Position', [pos(1), pos(2), pos(3)*1.5, pos(4)]);
+	draw_amplitude(x_averaged, y_averaged, fs);
+
+	if (sf.param.save_graphs_to_files)
+		H2 = figure("visible", "off");
+	else
+		H2 = figure;
+		pos = get(H2, 'Position');
+		set(H2, 'Position', [pos(1), pos(2), pos(3)*1.5, pos(4)*1.5]);
+		subplot(2, 2, 1);
+	endif
+	draw_whole_spectrum(x_reduced, fs, "Močnostni spekter izvirnega posnetka");
+
+	if (sf.param.save_graphs_to_files)
+		H3 = figure("visible", "off");
+	else
+		subplot(2, 2, 2);
+	endif
+	draw_whole_spectrum(y_reduced, fs, "Močnostni spekter predelanega posnetka");
+
+	if (sf.param.save_graphs_to_files)
+		H4 = figure("visible", "off");
+	else
+		subplot(2, 2, 3);
+	endif
+	draw_spectrogram(x_reduced, fs, "Spektrogram izvirnega posnetka");
+
+	if (sf.param.save_graphs_to_files)
+		H5 = figure("visible", "off");
+	else
+		subplot(2, 2, 4);
+	endif
+	draw_spectrogram(y_reduced, fs, "Spektrogram predelanega posnetka");
+
+	printf("# čas analize = %.3f s\n", toc);
+
+	if (sf.param.save_graphs_to_files)
+		img_settings = ["-r80"];
+
+		disp("# shranjujem v slikovne datoteke");
+		print(H1, "-r90", "sf_ampl.png");
+		print(H2, img_settings, "sf_spekter_x.png");
+		print(H3, img_settings, "sf_spekter_y.png");
+		print(H4, img_settings, "sf_spektrogram_x.png");
+		print(H5, img_settings, "sf_spektrogram_y.png");
+	endif
+endfunction
 
 % Izris amplitude dveh posnetkov na en graf v odvisnosti od časa
 function draw_amplitude(x, y, fs)
@@ -88,79 +164,3 @@ endfunction
 # 	% x, y, širina, višina
 	# set(gcf, 'Position', [pos(1), pos(2), pos(3)*2, pos(4)*2]);
 # end_try_catch
-
-close all;
-
-disp("-- Analiza signalov ---------");
-tic;
-
-% Če ima posnetek dva kanala, za izdelavo analize izračunaj njuno povprečje
-if (channels == 2)
-	x_averaged = (x(:,1) + x(:,2))/2;
-	y_averaged = (y(:,1) + y(:,2))/2;
-else
-	x_averaged = x;
-	y_averaged = y;
-endif
-
-if (numel(x)/fs > 60)
-	disp("# Omejujem dolžino posnetkov na 60 s pri FFT analizah");
-	x_reduced = x_averaged(1:60 * fs);
-	y_reduced = y_averaged(1:60 * fs);
-else
-	x_reduced = x_averaged;
-	y_reduced = y_averaged;
-endif
-
-if (SAV)
-	H1 = figure("visible", "off");
-else
-	H1 = figure;
-endif
-pos = get(H1, 'Position');
-set(H1, 'Position', [pos(1), pos(2), pos(3)*1.5, pos(4)]);
-draw_amplitude(x_averaged, y_averaged, fs);
-
-if (SAV)
-	H2 = figure("visible", "off");
-else
-	H2 = figure;
-	pos = get(H2, 'Position');
-	set(H2, 'Position', [pos(1), pos(2), pos(3)*1.5, pos(4)*1.5]);
-	subplot(2, 2, 1);
-endif
-draw_whole_spectrum(x_reduced, fs, "Močnostni spekter izvirnega posnetka");
-
-if (SAV)
-	H3 = figure("visible", "off");
-else
-	subplot(2, 2, 2);
-endif
-draw_whole_spectrum(y_reduced, fs, "Močnostni spekter predelanega posnetka");
-
-if (SAV)
-	H4 = figure("visible", "off");
-else
-	subplot(2, 2, 3);
-endif
-draw_spectrogram(x_reduced, fs, "Spektrogram izvirnega posnetka");
-
-if (SAV)
-	H5 = figure("visible", "off");
-else
-	subplot(2, 2, 4);
-endif
-draw_spectrogram(y_reduced, fs, "Spektrogram predelanega posnetka");
-
-printf("# čas analize = %.3f s\n", toc);
-
-if (SAV)
-	img_settings = ["-r80"];
-
-	disp("# shranjujem v slikovne datoteke");
-	print(H1, "-r90", "sf_ampl.png");
-	print(H2, img_settings, "sf_spekter_x.png");
-	print(H3, img_settings, "sf_spekter_y.png");
-	print(H4, img_settings, "sf_spektrogram_x.png");
-	print(H5, img_settings, "sf_spektrogram_y.png");
-endif
